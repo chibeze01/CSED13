0dca814cf3599f71f67a5ad71e9ee07123a713a0
Adam Shepherd <as3544@bath.ac.uk>
2020-03-24 20:00:22 +0000

    Initial importing of project into github

---
+++ SpendingTracker/src/spendingtracker/SpendingTracker.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spendingtracker;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author adam-
+ */
+public class SpendingTracker {
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        MainMenu menu = new MainMenu();
+        menu.setVisible(true);
+    }
+
+}
